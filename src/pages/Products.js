418e468d8c25cdbc4485ea89d154e60ca77a8177
Adrian Koša <adriankosa13@gmail.com>
2022-06-09 15:04:53 +0200

    first commit

---
+++ src/pages/Products.js
@@ -0,0 +1,7 @@
+const Products = () => {
+  return(
+    <h2>Welcome to products page</h2>
+  )
+}
+
+export default Products
---
+++ src/pages/Welcome.js
@@ -0,0 +1,7 @@
+const Welcome = () => {
+  return(
+    <h2>Welcome to welcome page</h2>
+  )
+}
+
+export default Welcome
